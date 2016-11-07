15d97cf5080f3b3ace2300c37af53bda9c674496
Treble <1062609615@qq.com>
2016-11-07 11:35:43 +0800

    add info for lng lat

--- math/lng_lat.py
+++ math/lng_lat.py
@@ -1,44 +1,44 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 from scipy.optimize import fsolve 
 from math import radians, cos, sin, asin, sqrt  
-import random
+import random, getopt, sys
   
-def haversine(lon1, lat1, lon2, lat2): # 经度1，纬度1，经度2，纬度2 （十进制度数）  
-	""" 
-	Calculate the great circle distance between two points  
-	on the earth (specified in decimal degrees) 
-	"""  
-	# 将十进制度数转化为弧度  
-	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])  
+def getlonlat(lon,lat,lenth):
+	haversine_format = '''
+def haversine(x): \n
+	lon1 = %s \n
+	lat1 = %s  \n
+	lon2 = float(x[0]) \n
+	lat2 = float(x[1]) \n
+	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2]) \n
+	\n
+	# haversine公式 \n
+	dlon = lon2 - lon1  \n
+	dlat = lat2 - lat1  \n
+	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2  \n
+	c = 2 * asin(sqrt(a))  \n
+	r = 6371.004  #地球平均半径，单位为公里  \n
 	
-	# haversine公式  
-	dlon = lon2 - lon1   
-	dlat = lat2 - lat1   
-	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2  
-	c = 2 * asin(sqrt(a))   
-	r = 6371.004 # 地球平均半径，单位为公里  
-	return c * r * 1000 
+	return [c * r * 1000 - %s, 3<lat2 <53 and 73<lon2<135]''' % (lon,lat,lenth)
+	exec(haversine_format)
+	return fsolve(haversine, [100, 28])
 
-#haversine(106.54637,29.592127,106.539471,29.587887)
+if __name__ == '__main__':
 
-def f(x):
-	lon1 = 106.54637
-	lat1 = 29.592127
-	lon2 = float(x[0])
-	lat2 = float(x[1])
-	lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "h",["help"]) 
+	except getopt.GetoptError as err:
+		print str(err)	
+		sys.exit() 
 
-    # haversine公式
-	dlon = lon2 - lon1
-	dlat = lat2 - lat1
-	a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-	c = 2 * asin(sqrt(a))
-	r = 6371.004
+	for op, value in opts:
+		if op in ("-h","--help"):
+			print "------Help-----\nFunction:  find a lon and lat based existed lon/lat and lenth\nFormat:  getlonlat(lon,lat,lenth(unit:m)) e.g. 106.54637,29.592127,3000"
+			sys.exit()
+		else: 
+			pass
+	print getlonlat(*args)
+#	print getlonlat(106.54637,29.592127,3000)
 
-	return [c * r * 1000 - 500,
-			3<lat2 <53 and 73<lon2<135]
-
-result = fsolve(f, [100, 28])
-
-print result
