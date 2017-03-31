e3dd62a5ae2e75eb5454e778a57667e2d5576b20
Treble <mengmengmeng@f-road.com.cn>
2017-03-31 17:22:14 +0800

    create db

--- interface/db.py
+++ interface/db.py
@@ -12,15 +12,14 @@ class BasicMysql:
 		self.user = kw['user']
 		self.password = kw['password']
 
-	def get_sql_conn(self):
 		try:
-			database_conn = MySQLdb.connect(host=self.host,port=self.port,user=self.user,passwd=self.password)
+			database_conn = MySQLdb.connect(host=self.host,port=self.port,user=self.user,passwd=self.password,local_infile = 1,charset="utf8")
 		except MySQLdb.Error, e:
 			sys.exit(e)
-		return database_conn
+		self.conn = database_conn
 
-	def output_result(self,query):
-		conn = self.get_sql_conn()
+	def exec_insert(self,query):
+		conn = self.conn
 		cursor = conn.cursor()
 
 		try:
@@ -28,49 +27,16 @@ class BasicMysql:
 		except Exception,e:
 			print Exception,":",e
 
-		oneline =  cursor.fetchone()
-
-		#output = open("aa",'w')  
-		#output.write(json.dumps(oneline,encoding="UTF-8", ensure_ascii=False))  
-		#output.close()  
-
+		conn.commit()
 		cursor.close()
-		conn.close
 		
-		return oneline
+#		return oneline
 
-class MerchantMysql(BasicMysql):
-	def __init__(self,host,port,user,passwd,database):
-		BasicMysql.__init__(self,host,port,user,passwd)
-		self.database = database
-
-	def get_merchant_passwd(self,username):
-		conn = BasicMysql.get_sql_conn(self)
-		cursor = conn.cursor()
-		query = 'select password from %s.cb_merchant_user where username="%s";' % (self.database,username)
+	def __exit__(self):
 		try:
-			cursor.execute(query)
+			self.conn.close
 		except Exception,e:
-			print Exception,":",e
-			sys.exit("load data bad")
-	
-		passwd_exist = cursor.fetchone()
-		cursor.close()
-
-		if passwd_exist == None:
-			sys.exit("can't get merchant passwd")
-		else:
-			passwd = passwd_exist[0]
-
-		return passwd	
+			 print Exception,":",e
 
 if __name__ == '__main__':
-	
-	#conn = get_sql_conn("10.43.2.6",3306,"root","root")
-	#print get_merchant_passwd(conn,"froad_cbank_2","4541501")
-	#MerchantMysqlTest = MerchantMysql("10.43.2.6",3306,"root","root","froad_cbank_2")
-	#MerchantMysqlTest.get_merchant_passwd("4541501")	
-	mysql_db = {"test2":{"ip":"10.43.2.7","port":3306,"user":"root","password":"root","db":"froad_cbank_3"}}
-	db = BasicMysql(host=mysql_db["test2"]["ip"],port=mysql_db["test2"]["port"],user=mysql_db["test2"]["user"],password=mysql_db["test2"]["password"])
-	query = "select code from %s where mobile='%s' ORDER BY create_time desc LIMIT 1;" %(mysql_db["test2"]["db"]+".cb_sms_log",'13916455521')
-	print	db.output_result(query)[0]
+	pass	
--- spark/create_data.py
+++ spark/create_data.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
+import sys
 import time
 import random
 import uuid
+
+path = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.join(path,"../"))
+from interface.db import BasicMysql
 
 
 if __name__ == "__main__":
 	start_time_stamp = time.mktime(time.strptime("2014-01-01 00:00:00","%Y-%m-%d %H:%M:%S"))
 	end_time_stamp = time.mktime(time.strptime("2015-03-01 00:00:00","%Y-%m-%d %H:%M:%S"))
     
-	product_name_list = ["苹果","橘子","香蕉","大米","小米","鱼","肉","牛肉"]
+	product_name_list = ["rice","peach","Pear","Banana","plum","orange"]
 
-	myfile = file("/tmp/1.txt","w+")
-	for i in xrange(0,3):
+	filename = "/tmp/1.txt"
+	myfile = file(filename,"w+")
+	for i in xrange(1,1000):
 		my_stamp = random.randint(int(start_time_stamp), int(end_time_stamp))
 		pay_time = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(my_stamp))
 
 		product_name = random.choice(product_name_list)
 		product_id = uuid.uuid1()
 		price = random.randint(100,150)
-		line = ",%s,%s,%s,%s\n" % (product_name,product_id,pay_time,price)
+		line = "%s,%s,%s,%s,%s\n" % (i,product_name,product_id,pay_time,price)
 		myfile.write(line)
 	myfile.close()
+
+	db = BasicMysql(host="127.0.0.1",port=3306,user="root",password="123456")
+	query = "load data local infile '%s' replace into table spark_test.product  fields terminated by ',';" % filename
+	db.exec_insert(query)
 
